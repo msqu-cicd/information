20925e4ed3c216fe25c68e6a3bada0a6a354acec
David Hiendl <david.hiendl@dhswt.de>
2023-12-02 17:45:45 +0100

    removed node_modules, started using vercel/ncc bundler

--- action.js
+++
@@ -1,9 +0,0 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
-import * as process from 'process';
-import * as semverParser from 'semver-parser';
-
-
-import * as gather from './gather';
-
-gather.gather_information(true, true);
